da4c1b6925835396f0c2d65156429a4437c64af6
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2025-11-22 11:10:20 +0530

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar,Welcome } from "#components";
+import { Dock,Navbar,Welcome } from "#components";
 
 const App=() =>{
 
@@ -7,6 +7,7 @@ const App=() =>{
     <main>
       <Navbar/>
       <Welcome/>
+      <Dock/>
     </main>
   );
 };
